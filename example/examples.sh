1c33f754bdf47dd9ca1437b22b44e38938585fa7
rwdavies <robertwilliamdavies@gmail.com>
2021-03-31 15:05:33 +0100

    Change examples slightly

--- example/examples.sh
+++ example/examples.sh
@@ -5,6 +5,8 @@ set -e
 script_dir=`dirname "$0"`
 cd "${script_dir}"/../
 
+MARKDOWN_FILE="${1:-example/QUILT_usage.Md}"
+
 ## get start and end bits
 lines_temp_file=$(mktemp)
 grep -n "\`\`\`" example/README.Md > ${lines_temp_file}
@@ -19,7 +21,7 @@ do
     a2=`echo $((2*${i} - 0))`
     b1=`head -n ${a1} ${lines_temp_file}| tail -n 1 | cut -f1 --delimiter=":"`
     b2=`head -n ${a2} ${lines_temp_file}| tail -n 1 | cut -f1 --delimiter=":"`    
-    awk '{if((NR > '${b1}') && (NR < '${b2}')) {print $0}}' example/README.Md >> ${script_temp_file}
+    awk '{if((NR > '${b1}') && (NR < '${b2}')) {print $0}}' ${MARKDOWN_FILE} >> ${script_temp_file}
 done
 
 ## hack, get rid of extra print out things
