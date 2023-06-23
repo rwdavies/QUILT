a6328ffbeb84e4e9d7377cd00708752fee5fee29
rwdavies <robertwilliamdavies@gmail.com>
2023-06-23 14:45:59 +0100

    Update STITCH version with latest needed things

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -14,7 +14,7 @@ if (!suppressPackageStartupMessages(require("rrbgen")))
     install.packages("https://github.com/rwdavies/rrbgen/raw/master/releases/rrbgen_0.0.4.tar.gz", repos=NULL)
 
 if (!suppressPackageStartupMessages(require("STITCH")))
-    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.6/STITCH_1.6.6.tar.gz", repos=NULL)
+    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.10/STITCH_1.6.10.tar.gz", repos=NULL)
 
 
 
