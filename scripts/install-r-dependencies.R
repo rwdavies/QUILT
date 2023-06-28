54f52fb154caf7b216f251dba8a5515451f089e2
rwdavies <robertwilliamdavies@gmail.com>
2023-06-28 12:18:56 +0100

    try to install latest stitch in tests

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -13,8 +13,25 @@ for(package in required_packages) {
 if (!suppressPackageStartupMessages(require("rrbgen")))
     install.packages("https://github.com/rwdavies/rrbgen/raw/master/releases/rrbgen_0.0.4.tar.gz", repos=NULL)
 
-if (!suppressPackageStartupMessages(require("STITCH")))
-    install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.10/STITCH_1.6.10.tar.gz", repos=NULL)
+    check <- as.logical(Sys.getenv("DEV_STITCH")) == TRUE
+    if (is.na(check)) {
+        check <- FALSE
+    }
+    if (check) {
+        install_github("rwdavies/STITCH", subdir = "STITCH", upgrade = "never")
+    }
+
+if (!suppressPackageStartupMessages(require("STITCH"))) {
+    check <- as.logical(Sys.getenv("DEV_STITCH")) == TRUE
+    if (is.na(check)) {
+        check <- FALSE
+    }
+    if (check) {
+        install_github("rwdavies/STITCH", subdir = "STITCH", upgrade = "never")
+    } else {
+        install.packages("https://github.com/rwdavies/STITCH/releases/download/1.6.10/STITCH_1.6.10.tar.gz", repos=NULL)
+    }
+}
 
 
 
