5c278be5fed316b84655d68247d5bfde03239630
Zilong-Li <zimusen94@gmail.com>
2024-02-28 08:59:58 +0100

    add scripts

--- scripts/zilong-test-nipt.R
+++ scripts/zilong-test-nipt.R
@@ -2,8 +2,7 @@ ANALYSIS_DIR <- "/home/zilong/.tmp/quilt_nipt_test"
 REF_PREFIX <- "1KGP"
 CHR <- "chr20"
 REGIONSTART <- 15281340
-REGIONEND <- 23023517
-REGIONSTART <- 18281340
+## REGIONEND <- 23023517
 REGIONEND <- 20023517
 BUFFER <- 500000
 FETAL_FRACTION <- 0.2
@@ -19,6 +18,7 @@ REGIONNAME <- paste0(CHR, ".", REGIONSTART, ".", REGIONEND)
 REGIONSUBSET <- paste0(CHR, ":", REGIONSTART - BUFFER, "-", REGIONEND + BUFFER)
 outputdir <- "quilt_output"
 outputdir2 <- "quilt_output2"
+outputdir3 <- "quilt_output3"
 ## choose ref panel here
 ## REF_PREFIX <- "ONEKG"
 ##REF_PREFIX <- "hrc"
@@ -137,7 +137,6 @@ source("~/Projects/github/QUILT/QUILT/R/gibbs-nipt.R")
 set.seed(SEED) ## do it again
 
 for(i_sample in 1:length(samples_to_use)) {
-
     sample <- samples_to_use[i_sample]
     load(file.path(outputdir, "input", paste0("sample.",i_sample, ".input.", REGIONNAME, ".RData")))
     load(file.path(outputdir, "input", paste0("sampleReadsInfo.",i_sample, ".input.", REGIONNAME, ".RData")))
@@ -173,7 +172,6 @@ for(i_sample in 1:length(samples_to_use)) {
     run("awk 'FNR==NR {a[$1]; next} FNR > 1 && $1 in a' ", file1, " ", file2, " >> ", file3)
     ## put into temporary bam file
     run("cat ", file3, " | samtools view -b > ", sample, ".prepped.", REGIONNAME, ".bam")
-    
 }
 
 
@@ -206,6 +204,15 @@ run("samtools index nipt.", REGIONNAME, ".bam")
 ##
 ## prepare a region to analyse
 
+## can either do full or downsampled versions
+## not entirely sure 
+run("samtools view -b -s 100.1666 nipt.", REGIONNAME, ".bam > nipt.downsampled.", REGIONNAME, ".bam")
+run("samtools view -b -s 100.999 nipt.", REGIONNAME, ".bam > nipt.downsampled.", REGIONNAME, ".bam")
+run("samtools index nipt.downsampled.", REGIONNAME, ".bam")
+cat("nipt.downsampled.", REGIONNAME, ".bam\n", sep = "", file = "bamlist.nipt.txt")
+fflist <- "fflist.nipt.txt"
+cat(FETAL_FRACTION, "\n", sep = "", file = "fflist.nipt.txt")
+
 ## exclude ref samples and anyone not in original 2504
 ##sample_to_keep <- setdiff(reference_samples[, 1], samples_to_use)
 ##samples_to_exclude <- reference_samples[!reference_samples[, 1] %in% sample_to_keep, 1]
@@ -215,6 +222,12 @@ write.table(matrix(samples_to_use, ncol = 1), file = "ref.samples.exclude.specif
 reference_vcf_file_exclude_target <- paste0(REF_PREFIX, ".uniq.vcf.gz")
 ## run("bcftools view -S ^ref.samples.exclude.specific.txt ", reference_vcf_file, " -Oz -o ", reference_vcf_file_exclude_target)
 ## run("bcftools index -f ", reference_vcf_file_exclude_target)
+
+## seems to work
+make_plots <- FALSE
+make_plots_block_gibbs <- FALSE
+nGibbsSamples <- 7
+n_seek_its <- 3
 
 QUILT_prepare_reference(
     outputdir = outputdir2,
@@ -226,27 +239,14 @@ QUILT_prepare_reference(
     regionStart= REGIONSTART,
     regionEnd = REGIONEND,
     buffer = BUFFER,
+    use_mspbwt = FALSE,
+    impute_rare_common = FALSE,
     rare_af_threshold = 0.001
 )
 
-
-## can either do full or downsampled versions
-## not entirely sure 
-run("samtools view -b -s 100.1666 nipt.", REGIONNAME, ".bam > nipt.downsampled.", REGIONNAME, ".bam")
-run("samtools view -b -s 100.999 nipt.", REGIONNAME, ".bam > nipt.downsampled.", REGIONNAME, ".bam")
-run("samtools index nipt.downsampled.", REGIONNAME, ".bam")
-cat("nipt.downsampled.", REGIONNAME, ".bam\n", sep = "", file = "bamlist.nipt.txt")
-fflist <- "fflist.nipt.txt"
-cat(FETAL_FRACTION, "\n", sep = "", file = "fflist.nipt.txt")
-
-## seems to work
-fflist <- "fflist.nipt.txt"
-make_plots <- FALSE
-make_plots_block_gibbs <- FALSE
-nGibbsSamples <- 5
-n_seek_its <- 3
 QUILT(
     outputdir = outputdir2,
+    prepared_reference_filename = file.path(outputdir2, "RData", paste0("QUILT_prepared_reference.", REGIONNAME, ".RData")),
     chr = CHR,
     regionStart= REGIONSTART,
     regionEnd = REGIONEND,
@@ -254,124 +254,87 @@ QUILT(
     nGen = 100,
     bamlist = "bamlist.nipt.txt",
     posfile = "pos.txt",
-    prepared_reference_filename = file.path(outputdir2, "RData", paste0("QUILT_prepared_reference.", REGIONNAME, ".RData")),
     method = "nipt",
-    fflist = fflist,
-    nGibbsSamples = nGibbsSamples,
+    fflist = "fflist.nipt.txt",
     phasefile = "phasefile.txt",   
+    nGibbsSamples = nGibbsSamples,
     n_seek_its = n_seek_its,
     make_plots = make_plots,
     make_plots_block_gibbs = make_plots_block_gibbs
 )
-## 
+
+## [2024-02-27 20:52:26] Final imputation accuracy for sample sim mat r2:0.969, PSE:0.2%, disc:2.6%
+## [2024-02-27 20:52:26] Final imputation accuracy for sample sim fet r2:0.89, PSE:0.3%, disc:11.9%
+## [2024-02-27 20:52:26] Final imputation accuracy for sample sim hap r2 mt:0.736, mu:0.628, pt:0.815
+
+## [2024-02-27 13:47:18] Final imputation accuracy for sample sim mat r2:0.953, PSE:0.1%, disc:3.9%
+## [2024-02-27 13:47:18] Final imputation accuracy for sample sim fet r2:0.813, PSE:0.1%, disc:6%
+## [2024-02-27 13:47:18] Final imputation accuracy for sample sim hap r2 mt:0.97, mu:0.889, pt:0.75
+
+
+QUILT_prepare_reference(
+    outputdir = outputdir3,
+    chr = CHR, 
+    nGen = 100,
+    reference_vcf_file = reference_vcf_file,
+    reference_exclude_samplelist_file = "ref.samples.exclude.specific.txt",
+    genetic_map_file = genetic_map_file,
+    regionStart= REGIONSTART,
+    regionEnd = REGIONEND,
+    buffer = BUFFER,
+    use_mspbwt = TRUE,
+    mspbwt_nindices = 1,
+    impute_rare_common = FALSE,
+    rare_af_threshold = 0.01
+)
+
+
+make_plots <- FALSE
+make_plots_block_gibbs <- FALSE
+nGibbsSamples <- 7
+n_seek_its <- 3
+
+QUILT(
+    outputdir = outputdir3,
+    prepared_reference_filename = file.path(outputdir3, "RData", paste0("QUILT_prepared_reference.", REGIONNAME, ".RData")),
+    chr = CHR,
+    regionStart= REGIONSTART,
+    regionEnd = REGIONEND,
+    buffer = BUFFER,
+    nGen = 100,
+    bamlist = "bamlist.nipt.txt",
+    posfile = "pos.txt",
+    method = "nipt",
+    fflist = "fflist.nipt.txt",
+    phasefile = "phasefile.txt",   
+    nGibbsSamples = nGibbsSamples,
+    n_seek_its = n_seek_its,
+    use_mspbwt = TRUE,
+    mspbwtM = 1,
+    mspbwtL = 10,
+    make_plots = make_plots,
+    make_plots_block_gibbs = make_plots_block_gibbs
+)
+
+# mspbwt impute_rare_common=false, mspbwt_nindicies=4
+## [2024-02-27 21:26:31] Final imputation accuracy for sample sim mat r2:0.962, PSE:0.3%, disc:2.8%
+## [2024-02-27 21:26:31] Final imputation accuracy for sample sim fet r2:0.891, PSE:0.1%, disc:13.2%
+## [2024-02-27 21:26:31] Final imputation accuracy for sample sim hap r2 mt:0.842, mu:0.766, pt:0.78
+
+# mspbwt impute_rare_common=false, mspbwt_nindicies=1
+## [2024-02-27 21:47:37] Final imputation accuracy for sample sim mat r2:0.956, PSE:0.6%, disc:11.4%
+## [2024-02-27 21:47:37] Final imputation accuracy for sample sim fet r2:0.857, PSE:0.3%, disc:24.6%
+## [2024-02-27 21:47:37] Final imputation accuracy for sample sim hap r2 mt:0.682, mu:0.346, pt:0.448
+
+# mspbwt impute_rare_common=true, mspbwt_nindicies=4, rare_af_threshold=0.01
+## [2024-02-27 21:12:01] Final imputation accuracy for sample sim mat r2:0.958, PSE:0.2%, disc:3.7%
+## [2024-02-27 21:12:01] Final imputation accuracy for sample sim fet r2:0.849, PSE:0.5%, disc:17%
+## [2024-02-27 21:12:01] Final imputation accuracy for sample sim hap r2 mt:0.523, mu:0.407, pt:0.65
+
+# mspbwt impute_rare_common=true, mspbwt_nindicies=1, rare_af_threshold=0.01
+## [2024-02-27 21:34:46] Final imputation accuracy for sample sim mat r2:0.961, PSE:0.4%, disc:3.7%
+## [2024-02-27 21:34:46] Final imputation accuracy for sample sim fet r2:0.858, PSE:0.2%, disc:14.6%
+## [2024-02-27 21:34:46] Final imputation accuracy for sample sim hap r2 mt:0.752, mu:0.554, pt:0.536
 
 
 
-plot.read.assignment <- function(){
-  par(mar = c(0, 0, 3, 0))
-  ylim <- c(0, 1)
-  plot(x = L_grid[1], y = 0, xlim = xlim, ylim = ylim, axes = FALSE, cex = 1.5, col = "white")
-  if (have_truth_haplotypes) {
-    truth <- truth_labels
-    truth[uncertain_truth_labels] <- 0
-    if (method == "nipt") {
-      label <- "Orange = truth hap 1, Blue = truth hap 2, Green = truth hap 3"
-    } else {
-      label <- "Orange = truth hap 1, Green = truth hap 2"
-    }
-    text(
-      x = L[1], y = (1),
-      labels = label,
-      pos = 4, cex = 1.25, xpd = NA
-    )
-  }
-  if (method == "nipt") {
-    y <- 0.1 + (2 - (read_labels - 1)) / 3 + runif(length(read_labels)) / 4
-  } else {
-    y <- 0.1 + (read_labels - 1) / 3 + runif(length(read_labels)) / 4
-  }
-  lwd <- 1.5
-  if (have_truth_haplotypes) {
-    col <- cbPalette[truth + 1]
-  } else {
-    col <- rep(cbPalette[1], nReads)
-  }
-  if (only_plot_confident_reads & have_truth_haplotypes) {
-    w <- truth != 0
-    segments(x0 = L[uu[1, !w]], x1 = L[uu[2, !w]], y0 = y[!w], y1 = y[!w], col = alpha_col("grey", 0.4), lwd = lwd)
-  } else {
-    w <- rep(TRUE, nReads)
-  }
-  segments(x0 = L[uu[1, w]], x1 = L[uu[2, w]], y0 = y[w], y1 = y[w], col = col[w], lwd = lwd)
-}
-
-load("/home/zilong/.tmp/RData/quilt2.nipt.1.1.RData")
-read_labels <- out$double_list_of_ending_read_labels[[1]][[1]] ## 1, 2, maybe 3 in the future
-y <- 0.1 + (2 - (read_labels - 1)) / 3 + runif(length(read_labels)) / 4
-plot(y)
-
-xlim <- range(L_grid)
-ylim <- c(0, 1)
-frac_reads <- 3 / 4
-frac_reads <- 2 / 3 
-
-plot(x = L_grid[1], y = 0, xlim = xlim, ylim = ylim, axes = FALSE, main = "", cex = 1.5, col = "white", cex.main = 1.5, xlab= "", ylab = "")
-
-## put these at the top
-y <- rep((1 - frac_reads), length(read_labels))
-H <- read_labels
-y[H == 1] <- y[H == 1] + (frac_reads) * (4 / 5 + runif(sum(H == 1)) * 1 / 5)
-y[H == 2] <- y[H == 2] + (frac_reads) * (2 / 5 + runif(sum(H == 2)) * 1 / 5)
-y[H == 3] <- y[H == 3] + (frac_reads) * (0 / 5 + runif(sum(H == 3)) * 1 / 5)
-text(x = xlim[1], y = (1 - frac_reads) + frac_reads * (9 / 10), labels = "Hap1", srt = 90, cex = 1.5, font = 2)
-text(x = xlim[1], y = (1 - frac_reads) + frac_reads * (5 / 10), labels = "Hap2", srt = 90, cex = 1.5, font = 2)
-text(x = xlim[1], y = (1 - frac_reads) + frac_reads * (1 / 10), labels = "Hap3", srt = 90, cex = 1.5, font = 2)
-
-us <- t(sapply(1:length(sampleReads), function(iRead) {
-  range(sampleReads[[iRead]][[4]])
-}))
-
-## level depending on what I say
-## colour dpeending on truth
-## grey <- alpha_col("grey", 0.25)
-grey <- "#999999"
-col <- rep(grey, length(read_labels))
-col <- c(grey, "orange", "green", "blue")
-col <- c(grey, "#F2B342", "#E7872B", "#317EC2")
-
-## over-plot coloured ones
-
-png("initial-reads.png", w=4, h = 6, units="in", res=300)
-M <- 10000
-lwd <- 4
-par(mar=rep(0,4))
-plot(x = L_grid[1], y = 0, xlim = range(L[1:M]), ylim = ylim, axes = FALSE, main = "", cex = 1.5, col = "white", cex.main = 1.5, xlab= "", ylab = "")
-col <- c(grey,"#317EC2", "#F2B342", "#E7872B")
-y <- (2 - (read_labels - 1)) / 3 + runif(length(read_labels)) / 4
-for(i in 1:2) {
-  if (i == 1) w <- col == grey
-  if (i == 2) w <- col != grey
-  if (i==2) M <- 3 *M
-  segments(x0 = L[us[w, 1] + 1][1:M], x1 = L[us[w, 2] + 1][1:M], y0 = y[w][1:M], y1 = y[w][1:M], col = col[w], lwd = lwd)
-}
-dev.off()
-
-
-plot(x = L_grid[1], y = 0, xlim = range(L[1:M]), ylim = ylim, axes = FALSE, main = "", cex = 1.5, col = "white", cex.main = 1.5, xlab= "", ylab = "")
-w <- as.logical(c(1,0,0,0))
-segments(x0 = L[us[w, 1] + 1][1:M], x1 = L[us[w, 2] + 1][1:M], y0 = y[w][1:M], y1 = y[w][1:M], col = col[w], lwd = lwd)
-w <- as.logical(c(0,1,0,0))
-segments(x0 = L[us[w, 1] + 1][1:M], x1 = L[us[w, 2] + 1][1:M], y0 = y[w][1:M], y1 = y[w][1:M], col = col[w], lwd = lwd)
-
-x0 = L[us[w, 1] + 1]
-x1 = L[us[w, 2] + 1]
-head(cbind(x0,x1))
-dim(us[w,])
-length(L_grid)
-
-label0 <- L[us[as.logical(1,0,0,0), ] + 1]
-label1 <- L[us[as.logical(0,1,0,0), ] + 1]
-label2 <- L[us[as.logical(0,0,1,0), ] + 1]
-label3 <- L[us[as.logical(0,0,0,1), ] + 1]
-
-colorpalette
