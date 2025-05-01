32859fde53a499603844d21b0f15150e80a13903
Zilong-Li <zimusen94@gmail.com>
2025-05-01 16:32:47 +0200

    update dependency Rcpp==1.0.14

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 ### latest Rcpp 1.0.13 is not working
-install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_1.0.12.tar.gz", repos=NULL, type="source")
+install.packages("https://cran.r-project.org/src/contrib/Rcpp_1.0.14.tar.gz", repos=NULL, type="source")
 
 required_packages <- c("proftools", "RcppArmadillo", "optparse", "devtools", "testthat", "roxygen2", "data.table", "RcppEigen", "microbenchmark")
 for(package in required_packages) {
@@ -32,7 +32,7 @@ if (!suppressPackageStartupMessages(require("STITCH"))) {
     if (check) {
         install_github("rwdavies/STITCH", subdir = "STITCH", upgrade = "never")
     } else {
-        install.packages("https://github.com/rwdavies/STITCH/releases/download/1.7.0/STITCH_1.7.0.tar.gz", repos=NULL)
+        install.packages("https://github.com/rwdavies/STITCH/releases/download/1.7.3/STITCH_1.7.3.tar.gz", repos=NULL)
     }
 }
 
